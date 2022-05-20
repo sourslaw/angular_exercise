97cd5890283dbf99de64dcc11eee02e2761c9ca3
Scott Lawan <lawanscott@gmail.com>
2022-05-20 15:06:11 -0600

    added firebase db routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +7,20 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-
   // emitting an event (links)
   // @Output() featureSelected = new EventEmitter<string>(); //Output allows us to listent to the event from the parent component (app.component)
 
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   // }
+  constructor(private dataStorageService: DataStorageService) {}
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,35 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'a test schnitzel',
-      'this is a simple test desc.',
-      'https://images.unsplash.com/photo-1560611588-163f295eb145?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-      [
-        new Ingredient('meat', 1),
-        new Ingredient('french fries', 17)
-      ]),
-    new Recipe(
-      'another test burger',
-      'this is ANOTHER simple test desc.',
-      'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
-      [
-        new Ingredient('buns', 1),
-        new Ingredient('meat', 1),
-        new Ingredient('sauce', 1),
-        new Ingredient('tots', 9),
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'a test schnitzel',
+  //     'this is a simple test desc.',
+  //     'https://images.unsplash.com/photo-1560611588-163f295eb145?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  //     [
+  //       new Ingredient('meat', 1),
+  //       new Ingredient('french fries', 17)
+  //     ]),
+  //   new Recipe(
+  //     'another test burger',
+  //     'this is ANOTHER simple test desc.',
+  //     'https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
+  //     [
+  //       new Ingredient('buns', 1),
+  //       new Ingredient('meat', 1),
+  //       new Ingredient('sauce', 1),
+  //       new Ingredient('tots', 9),
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
